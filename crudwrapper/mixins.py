42cd7e7f45fa2cb489ab51daa044cd522d6ea935
Jerick Don San Juan <jerick@icannhas.com>
2014-05-29 06:58:01 +0800

    Created UserRelatedRequiredMixin

--- crudwrapper/mixins.py
+++ crudwrapper/mixins.py
@@ -2,7 +2,7 @@ from django.core.exceptions import ImproperlyConfigured
 from braces.views import LoginRequiredMixin, UserPassesTestMixin
 
 __all__ = (
-    'ModulePermRequiredMixin', 'CancelURLMixin'
+    'ModulePermRequiredMixin', 'CancelURLMixin',  'UserRelatedRequiredMixin'
 )
 
 
@@ -17,6 +17,35 @@ class ModulePermRequiredMixin(LoginRequiredMixin, UserPassesTestMixin):
                 "'module_name' (the app_label).")
 
         return user.has_module_perms(self.module_name)
+
+
+class UserRelatedRequiredMixin(UserPassesTestMixin):
+    """
+    Make sure that the current User has a relation with the object.
+    """
+
+    relation_field = None
+
+    def test_func(self, user):
+        if self.relation_field is None:
+            raise ImproperlyConfigured(
+                "'UserRelatedRequiredMixin' requires "
+                "'relation_field' (the model field related to request.user).")
+
+        obj_result = self.get_result_relation_field(self.get_object(), self.relation_field)
+
+        return obj_result == self.request.user
+
+    def get_result_relation_field(self, obj, relfield):
+        fields = relfield.split('__')
+        for field in fields:
+            try:
+                obj = getattr(obj, field)
+                if not obj:
+                    raise ImproperlyConfigured("Invalid relation field, returns None")
+            except AttributeError:
+                raise ImproperlyConfigured("Invalid relation_field '{}'".format(relfield))
+        return obj
 
 
 class CancelURLMixin(object):
